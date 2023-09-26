20c5dfe8dc4b878733fa9be0ec9eab482c1c26e4
github18122021 <vrajbagadiya0071@gmail.com>
2023-09-25 20:26:28 -0400

    Lab1 Completed, commenting remaining
    New files package-lock.json and package.json added

--- LAB01/lab1.js
+++ LAB01/lab1.js
@@ -1,1 +1,49 @@
-console.log("Hello World");
+var prompt = require("prompt");
+prompt.start();
+
+let userInput;
+prompt.get(["userInput"], function (err, result) {
+    if (err) {
+        console.error(`Error Message is: ${err}`);
+        return;
+    }
+    console.log("(1) Enter R for Rock");
+    console.log("(2) Enter P for Paper");
+    console.log("(3) Enter S for Scissor");
+    console.log("Enter your choice here: " + result.userInput);
+    userInput = result.userInput;
+
+    let computerInput = Math.random();
+    if (computerInput >= 0.0 && computerInput <= 0.34) {
+        computerInput = "P";
+    } else if (computerInput >= 0.35 && computerInput <= 0.67) {
+        computerInput = "S";
+    } else {
+        computerInput = "R";
+    }
+    console.log(`UserInput: ${userInput}`); //${typeof userInput} = string
+    console.log(`ComputerInput: ${computerInput}`); // ${typeof userInput} = string
+
+    let value = ["R", "P", "S"];
+    if (userInput === computerInput) {
+        console.log("It's a Tie");
+    } else if (userInput === "R") {
+        if (computerInput === "P") {
+            console.log("Computer Won");
+        } else {
+            console.log("User Won");
+        }
+    } else if (userInput === "P") {
+        if (computerInput === "S") {
+            console.log("Computer Won");
+        } else {
+            console.log("User Won");
+        }
+    } else if (userInput === "S") {
+        if (computerInput === "R") {    
+            console.log("Computer Won");
+        } else {
+            console.log("User Won");
+        }
+    }
+});
