b3ccd140de131264835e9f964fc48102a360e2f2
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2023-09-25 20:33:01 -0400

    Lab1 Completed

--- LAB01/lab1.js
+++ LAB01/lab1.js
@@ -1,7 +1,6 @@
 var prompt = require("prompt");
 prompt.start();
 
-let userInput;
 prompt.get(["userInput"], function (err, result) {
     if (err) {
         console.error(`Error Message is: ${err}`);
@@ -11,9 +10,10 @@ prompt.get(["userInput"], function (err, result) {
     console.log("(2) Enter P for Paper");
     console.log("(3) Enter S for Scissor");
     console.log("Enter your choice here: " + result.userInput);
-    userInput = result.userInput;
+    let userInput = result.userInput; // Storing userInput value in variable
 
-    let computerInput = Math.random();
+    let computerInput = Math.random(); // Taking a random number between 0.0 and 1.0
+    // Coverting computerInput into R,P,S
     if (computerInput >= 0.0 && computerInput <= 0.34) {
         computerInput = "P";
     } else if (computerInput >= 0.35 && computerInput <= 0.67) {
@@ -21,10 +21,11 @@ prompt.get(["userInput"], function (err, result) {
     } else {
         computerInput = "R";
     }
+    // Displaying the value of user and computer in console using String Literals
     console.log(`UserInput: ${userInput}`); //${typeof userInput} = string
     console.log(`ComputerInput: ${computerInput}`); // ${typeof userInput} = string
 
-    let value = ["R", "P", "S"];
+    // Checking WHO WINS using nested if loops
     if (userInput === computerInput) {
         console.log("It's a Tie");
     } else if (userInput === "R") {
@@ -40,7 +41,7 @@ prompt.get(["userInput"], function (err, result) {
             console.log("User Won");
         }
     } else if (userInput === "S") {
-        if (computerInput === "R") {    
+        if (computerInput === "R") {
             console.log("Computer Won");
         } else {
             console.log("User Won");
